b6c6dc5e9551221f8415fe5cd51db939bcb1cc6d
Erik Huemer <erikh@sbg.nic.at>
2025-04-24 11:18:02 +0200

    Update setup.py and add egg-info files

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name="bebop",
     version="0.1.0",
     packages=find_packages(),
+    package_data={
+        'app': ['*.py'],
+    },
+    include_package_data=True,
     install_requires=[
         'requests',
         'beautifulsoup4',
@@ -18,4 +22,9 @@ setup(
         'pyOpenSSL',
     ],
     python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'bebop=app.__main__:main',
+        ],
+    },
 ) 
